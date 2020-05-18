4cb25728fae6c945e18f45dd9ead0fa9f663cd7b
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-05-18 13:37:42 +0300

    Configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,37 @@
 module.exports = {
-  extends: "plugin:prettier/recommended",
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true,
+        "jest/globals": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:prettier/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly",
+        "React": "writable"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "jest"
+    ],
+    "rules": {
+        "react/react-in-js-scope": "off",
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error"
+    }
 };

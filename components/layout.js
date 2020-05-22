f8e6d71a85eddf1e339435e98d7fae77a48717c1
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-05-22 13:58:25 +0300

    Remove className container because it has a special meaning in tailwind

--- components/layout.js
+++ components/layout.js
@@ -10,7 +10,7 @@ function Layout({ user, loading = false, children }) {
 
       <Header user={user} loading={loading} />
 
-      <div className="container flex flex-col justify-center items-center min-h-screen">
+      <div className="flex flex-col justify-center items-center min-h-screen">
         {children}
       </div>
     </>

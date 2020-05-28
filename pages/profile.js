ee1765a34e08017dd0d4e2e4287e54583dcf83e8
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-05-28 14:25:17 +0300

    Customize profile page

--- pages/profile.js
+++ pages/profile.js
@@ -2,28 +2,37 @@
 // import auth0 from '../lib/auth0'
 import { useFetchUser } from "../lib/user";
 import Layout from "../components/layout";
+import PropTypes from "prop-types";
 
 function ProfileCard({ user }) {
   return (
     <>
-      <h1>Profile</h1>
+      <h1>Profiili</h1>
 
       <div>
         <h3>Profile (client rendered)</h3>
         <img src={user.picture} alt="user picture" />
         <p>nickname: {user.nickname}</p>
         <p>name: {user.name}</p>
+        {user.isAdmin() && (
+          <>
+            <p>Admin</p>
+          </>
+        )}
       </div>
     </>
   );
 }
+ProfileCard.propTypes = {
+  user: PropTypes.object,
+};
 
 function Profile() {
   const { user, loading } = useFetchUser({ required: true });
 
   return (
     <Layout user={user} loading={loading}>
-      {loading ? <>Loading...</> : <ProfileCard user={user} />}
+      {loading ? <>Ladataan...</> : <ProfileCard user={user} />}
     </Layout>
   );
 }

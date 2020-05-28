25b4e65309bd8144737ee09e954b325c2f16d93c
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-05-28 14:06:25 +0300

    Configure ennustin-gray

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   purge: ["./components/**/*.js", "./pages/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "ennustin-gray": {
+          lighter: "#444",
+          default: "#333",
+          darker: "#222",
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],

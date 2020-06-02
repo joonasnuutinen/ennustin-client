9cb65c7c9bbedccd8e218010f0345fa1494e44f9
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-06-02 16:06:26 +0300

    Only allow admin users to the phenomena page

--- pages/ilmiot.js
+++ pages/ilmiot.js
@@ -2,6 +2,49 @@ import { useFetchUser } from "../lib/user";
 import Layout from "../components/layout";
 //import PropTypes from "prop-types";
 import Head from "next/head";
+import Link from "next/link";
+
+function PhenomenaContent({ user }) {
+  return user.isAdmin() ? (
+    <>
+      <Head>
+        <title>Ilmiöt</title>
+      </Head>
+
+      <main>
+        <h1>Ilmiöt</h1>
+        <form>
+          <label>
+            Nimi
+            <input type="text" />
+          </label>
+          <label>
+            Käyttäjä
+            <select>
+              <option>Test 1</option>
+              <option>Test 2</option>
+              <option>Test 3</option>
+            </select>
+          </label>
+        </form>
+      </main>
+    </>
+  ) : (
+    <>
+      <Head>
+        <title>Pääsy kielletty</title>
+      </Head>
+      <main>
+        <h1>Pääsy kielletty</h1>
+        <p>
+          <Link href="/profiili">
+            <a className="text-blue-600">Siirry profiiliin</a>
+          </Link>
+        </p>
+      </main>
+    </>
+  );
+}
 
 function Phenomena() {
   const { user, loading } = useFetchUser({ required: true });
@@ -9,27 +52,11 @@ function Phenomena() {
   return (
     <>
       <Head>
-        <title>Ilmiöt</title>
+        <title>Ladataan...</title>
       </Head>
 
       <Layout user={user} loading={loading}>
-        <main>
-          <h1>Ilmiöt</h1>
-          <form>
-            <label>
-              Nimi
-              <input type="text" />
-            </label>
-            <label>
-              Käyttäjä
-              <select>
-                <option>Test 1</option>
-                <option>Test 2</option>
-                <option>Test 3</option>
-              </select>
-            </label>
-          </form>
-        </main>
+        {loading ? <>Ladataan...</> : <PhenomenaContent user={user} />}
       </Layout>
     </>
   );
